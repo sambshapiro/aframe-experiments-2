bddf50b2db8caf2ef609b885cc84a4db156cbeca
Sam Shapiro <sambshapiro@gmail.com>
2017-07-07 19:21:08 -0400

    testing avatar image template over socketio

---
+++ server/static/upload.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  document.getElementById("media_input").addEventListener("change", function() {
+    console.log("image uploaded!");
+    mediaLoader()
+  });
+
+
+});
+var reader = new FileReader();
+
+function mediaLoader() {
+  //Get the files from the input
+  var files = document.getElementById("media_input").files;
+  var file = document.getElementById("media_input").files[0];
+  var filetype;
+  for (var i = 0; i < files.length; i++) {
+    filetype = files[i].type;
+  }
+
+  reader.onload = function() { //when reader load->
+
+    if (filetype.includes('image')){
+
+      var image = new Image();
+
+      image.src = reader.result;
+
+      image.onload = function() { //IMAGE HAS BEEN LOADED
+        console.log("broadcasting data");
+        NAF.connection.broadcastData('avatar', image.src);
+        //document.getElementById("avatar-image").src = image.src;
+        //$("#avatar-image").attr('src', 'url(' + image.src + ')');
+        document.querySelector('a-scene').querySelector('.avatar-image-class').setAttribute('material', 'src: url(' + image.src + ')');
+      }
+    }
+    $('#media_input_div').hide();
+
+  }
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    console.log("Reader fail")
+  }
+
+
+}
