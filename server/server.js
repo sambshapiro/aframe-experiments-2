eb9b7b058ed9030d3abbd57a4e1da628c0d4fb00
Sam Shapiro <sambshapiro@gmail.com>
2017-07-12 22:49:17 -0400

    images saving to database and being populated upon new connection

--- server/server.js
+++ server/server.js
@@ -3,7 +3,19 @@ var http    = require("http");              // http server core module
 var express = require("express");           // web framework external module
 var serveStatic = require('serve-static');  // serve static files
 var socketIo = require("socket.io");        // web socket external module
-var easyrtc = require("easyrtc");               // EasyRTC external module
+var easyrtc = require("easyrtc");           // EasyRTC external module
+
+//var mongodb = require('mongodb');
+var config = require('../server/config');
+var mLab = 'mongodb://' + config.db.host + '/' + config.db.name;
+//var MongoClient = mongodb.MongoClient;
+//var collection;
+
+var fs = require("fs");
+var mongoose = require("mongoose");
+mongoose.connect(mLab);
+var conn = mongoose.connection;
+bodyParser = require('body-parser');
 
 // Set process name
 process.title = "node-easyrtc";
@@ -13,6 +25,7 @@ var port = process.env.PORT || 8080;
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var app = express();
+app.use(bodyParser.json({limit: '5mb'}));
 app.use(serveStatic('server/static', {'index': ['index.html']}));
 
 // Start Express http server
@@ -43,38 +56,68 @@ easyrtc.setOption("demosEnable", false);
 
 // Overriding the default easyrtcAuth listener, only so we can directly access its callback
 easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
-    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
-        if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
-            callback(err, connectionObj);
-            return;
-        }
+  easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
+    if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+      callback(err, connectionObj);
+      return;
+    }
 
-        connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
+    connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
 
-        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
+    console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
 
-        callback(err, connectionObj);
-    });
+    callback(err, connectionObj);
+  });
 });
 
 // To test, lets print the credential to the console for every room join!
 easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
-    easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
+  console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+  easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
 });
 
 // Start EasyRTC server
 var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
-    console.log("Initiated");
+  console.log("Initiated");
 
-    rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
-        console.log("roomCreate fired! Trying to create: " + roomName);
+  rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+    console.log("roomCreate fired! Trying to create: " + roomName);
 
-        appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
-    });
+    appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
+  });
 });
 
 //listen on port
 webServer.listen(port, function () {
-    console.log('listening on http://localhost:' + port);
+  console.log('listening on http://localhost:' + port);
+});
+
+conn.once("open", function(){
+
+  var imagesSchema = mongoose.Schema({
+    src: String,
+    position: mongoose.Schema.Types.Mixed,
+    rotation: mongoose.Schema.Types.Mixed
+  });
+  var imageModel = mongoose.model('imageModel', imagesSchema);
+
+  app.post("/imageUpload", function(req, res){
+    var newImage = new imageModel({ src: req.body.src, position: req.body.position, rotation: req.body.rotation });
+    newImage.save(function (err, newImage) {
+      if (err) return console.error(err);
+      console.log("image successfully added to database");
+    });
+    res.end();
+  });
+
+  app.get("/retrieveImages", function(req, res){
+    imageModel.find(function (err, images) {
+      if (err) return console.error(err);
+      console.log(images);
+      res.send(JSON.stringify(images));
+    });
+
+  });
+
+
 });
--- server/static/upload.js
+++ server/static/upload.js
@@ -1,13 +1,14 @@
+var firstUpload = true;
+var reader = new FileReader();
+
 $(document).ready(function(){
 
   document.getElementById("media_input").addEventListener("change", function() {
     console.log("image uploaded!");
-    mediaLoader()
+    mediaLoader();
   });
 
-
 });
-var reader = new FileReader();
 
 function mediaLoader() {
   //var files = document.getElementById("media_input").files;
@@ -24,36 +25,56 @@ function mediaLoader() {
 
     if (filetype == 'image/gif') {
 
-      var image = new Image();
+      console.log("broadcasting data");
+      var data = { src: reader.result, position: setPosition, rotation: setRotation };
+      //NAF.connection.broadcastData('imagePlaced', data);
+      //document.getElementById("avatar-image").src = image.src;
+      //$("#avatar-image").attr('src', 'url(' + image.src + ')');
+      document.querySelector('a-scene').querySelectorAll('.avatar-image-class')[0].setAttribute('material', {src: 'url(' + reader.result + ')', shader: "gif"});
 
-      image.src = reader.result;
-
-      image.onload = function() { //IMAGE HAS BEEN LOADED
-        console.log("broadcasting data");
-        NAF.connection.broadcastData('avatar', image.src);
-        //document.getElementById("avatar-image").src = image.src;
-        //$("#avatar-image").attr('src', 'url(' + image.src + ')');
-        document.querySelector('a-scene').querySelectorAll('.avatar-image-class')[0].setAttribute('material', {src: 'url(' + image.src + ')', shader: "gif"});
-      }
-
-      document.querySelector('#avatar-hud').setAttribute('material', {src: 'url(' + image.src + ')', shader: "gif"});
+      document.querySelector('#avatar-hud').setAttribute('material', {src: 'url(' + reader.result + ')', shader: "gif"});
     }
 
     else if (filetype.includes('image')){
 
-      var image = new Image();
+      console.log ("firstUpload " + firstUpload);
+      if (!firstUpload) {
+        var containerEl = document.createElement('a-entity');
+        var entityEl = document.createElement('a-image');
+        containerEl.appendChild(entityEl);
+        document.querySelector('a-scene').appendChild(containerEl);
+        entityEl.setAttribute('visible','true');
+        var currentPosition = document.querySelector('a-scene').querySelector('#player').getAttribute('position');
+        var currentRotation = document.querySelector('a-scene').querySelector('#player').getAttribute('rotation');
+        var setPosition = new THREE.Vector3(currentPosition.x+1, currentPosition.y, currentPosition.z+1);
+        var setRotation = currentRotation;
+        entityEl.setAttribute('position',setPosition);
+        entityEl.setAttribute('rotation',currentRotation);
+        var currentImageSrc = document.querySelector('a-scene').querySelectorAll('.avatar-image-class')[0].getAttribute('material').src;
+        entityEl.setAttribute('material', 'src', 'url(' + currentImageSrc + ')');
 
-      image.src = reader.result;
-
-      image.onload = function() { //IMAGE HAS BEEN LOADED
+        var data = { src: currentImageSrc, position: setPosition, rotation: setRotation };
         console.log("broadcasting data");
-        NAF.connection.broadcastData('avatar', image.src);
-        //document.getElementById("avatar-image").src = image.src;
-        //$("#avatar-image").attr('src', 'url(' + image.src + ')');
-        document.querySelector('a-scene').querySelectorAll('.avatar-image-class')[0].setAttribute('material', 'src', 'url(' + image.src + ')');
-      }
+        //NAF.connection.broadcastDataGuaranteed('imagePlaced', JSON.stringify(data));
 
-      document.querySelector('#avatar-hud').setAttribute('src', 'url(' + image.src + ')');
+        $.ajax({
+          type: 'POST',
+          data: JSON.stringify(data),
+          contentType: 'application/json',
+          url: 'http://localhost:8080/imageUpload',
+          success: function(data) {
+            console.log('success');
+            console.log(JSON.stringify(data));
+          }
+        });
+        //dbcollection_images.insert([newImage]);
+        //console.log("image added to database");
+      }
+      //document.getElementById("avatar-image").src = image.src;
+      //$("#avatar-image").attr('src', 'url(' + image.src + ')');
+      document.querySelector('a-scene').querySelectorAll('.avatar-image-class')[0].setAttribute('material', 'src', 'url(' + reader.result + ')');
+
+      document.querySelector('#avatar-hud').setAttribute('src', 'url(' + reader.result + ')');
       //var preview = document.createElement()
     }
 
@@ -63,6 +84,7 @@ function mediaLoader() {
     }
 
     $('#media_input_div').hide();
+    if (firstUpload) firstUpload = false;
 
   }
   if (file) {
